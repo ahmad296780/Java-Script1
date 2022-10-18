96bda993e946195f8084539fe03f5a9a13390da5
ahmad <bha296780@gmail.com>
2022-10-17 21:20:54 -0400

    final

---
+++ app.js
@@ -0,0 +1,27 @@
+// Exercise 1
+const firstname = Ahmad;
+let lastname;
+var age;
+// Exercise 2
+firstname = Ahmad;
+lastname= "Hassan";
+age= 23;
+// Exerceise 3
+let language = "javascript";
+let createdyear=1995;
+let isCaseSensitive =true;
+// Exercise 4
+let price=19.99;
+let isOnSale=false;
+let salePercentage=15;
+let stock=0;
+let inStock = false;
+let selectedSize="M";
+// Exercise5
+let tittle="Name of the wind";
+let author="Patrick Rothfuss";
+let pageCount=722;
+let bookmark=456;
+let hasRead=true;
+
+
